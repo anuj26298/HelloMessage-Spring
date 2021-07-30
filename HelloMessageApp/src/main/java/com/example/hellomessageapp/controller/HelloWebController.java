64787f9e6f1e530f689fd9eb1f9e496e4193b6e4
anuj26298 <kumar26298anuj@gmail.com>
2021-07-30 21:52:11 +0530

    ADDED- Display hello msg by taking name is query parameters

--- HelloMessageApp/src/main/java/com/example/hellomessageapp/controller/HelloWebController.java
+++ HelloMessageApp/src/main/java/com/example/hellomessageapp/controller/HelloWebController.java
@@ -1,11 +1,15 @@
 package com.example.hellomessageapp.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWebController {
+    @GetMapping("/web")
+        public String hello(){
+            return "hello";
+        }
     @GetMapping("/web/message")
     public String message(Model model){
         model.addAttribute("message", "Hello, How are you?");
--- HelloMessageApp/src/main/java/com/example/hellomessageapp/controller/MessageRestController.java
+++ HelloMessageApp/src/main/java/com/example/hellomessageapp/controller/MessageRestController.java
@@ -1,14 +1,20 @@
 package com.example.hellomessageapp.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/message")
 public class MessageRestController {
     @GetMapping("/hello")
     public String helloMessage(){
         return "Hello from Bridgelabz";
     }
+    @GetMapping("/helloName")
+    public String sayHello(@RequestParam String name){
+        return "Hello," + name;
+    }
+
 }
